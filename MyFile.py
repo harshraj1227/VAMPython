ceb544443b4d4bdc3aaa1ed9f55dbdce1f4d7997
harshraj1227 <harshkumar2762002@gmail.com>
2024-10-01 10:17:41 +0530

    code update

---
+++ MyFile.py
@@ -0,0 +1,20 @@
+# #Create file in python with name and extension
+# MyFile= open("harsh.txt", "w")
+# #write name and email in harsh text file.
+# MyFile.write("My name is Harsh kumar and my e-mail is hk@gmail.com")
+# # to close the file
+# MyFile.close()
+
+# #read the data from file
+# readFile = open("harsh.txt", "r")
+# # print(readFile.read())
+
+# #delete the file
+# import os
+
+# os.remove("harsh.txt")
+
+#create stock api in json--> java script object notation
+
+myStock = open("mystock.json","w")
+myStock.write("this is my stock api")
