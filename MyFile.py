b7fe506fea7a9898e5e263e20f155a20787e2e1f
harshraj1227 <harshkumar2762002@gmail.com>
2024-10-03 19:45:11 +0530

    update

--- MyFile.py
+++ MyFile.py
@@ -1,18 +1,18 @@
-# #Create file in python with name and extension
-# MyFile= open("harsh.txt", "w")
-# #write name and email in harsh text file.
-# MyFile.write("My name is Harsh kumar and my e-mail is hk@gmail.com")
-# # to close the file
-# MyFile.close()
+#Create file in python with name and extension
+MyFile= open("harsh.txt", "w")
+#write name and email in harsh text file.
+MyFile.write("My name is Harsh kumar and my e-mail is hk@gmail.com")
+# to close the file
+MyFile.close()
 
-# #read the data from file
-# readFile = open("harsh.txt", "r")
-# # print(readFile.read())
+#read the data from file
+readFile = open("harsh.txt", "r")
+# print(readFile.read())
 
-# #delete the file
-# import os
+#delete the file
+import os
 
-# os.remove("harsh.txt")
+os.remove("harsh.txt")
 
 #create stock api in json--> java script object notation
 
