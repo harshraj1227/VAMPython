482150a8626e397ef3d61bbeb788137b5fe0ce1c
harshraj1227 <harshkumar2762002@gmail.com>
2024-10-07 09:36:41 +0530

    code update

--- program.py
+++ program.py
@@ -1,12 +1,24 @@
-#Age Counting
-#bornYear= int(input("enter your born year"))
-#currentYear=int(input("enter current year"))
+# #Age Counting
+# bornYear= int(input("enter your born year"))
+# currentYear=int(input("enter current year"))
 
-#userage = currentYear - bornYear
-#print("Your age is", userage)
+# userage = currentYear - bornYear
+# print("Your age is", userage)
 
-#Currency Conversion
-INR=int(input("enter no of USD "))
-conversion= 84*INR
+# #Currency Conversion
+# INR=int(input("enter no of USD "))
+# conversion= 84*INR
 
-print("after conversion: ", conversion)
+# print("after conversion: ", conversion)
+
+#create their first Python project to generate random 6 digit OTP
+import random  
+
+def generate_otp(length=6):  
+     
+    otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])  
+    return otp  
+
+if __name__ == "__main__":  
+    otp = generate_otp()  
+    print(f"Your OTP is: {otp}")
